e51d127b22fa5a27f4f25b4c82021ee7e4659dff
Paul <paul.bodineau@etu.univ-nantes.fr>
2026-03-12 09:38:28 +0100

    Objets interactifs fini et fonctionnels

--- metropolegrandlyon/public/js/interactive.js
+++ metropolegrandlyon/public/js/interactive.js
@@ -1,14 +1,10 @@
+//Liste de tous les éléments intéractifs de la page
 const list_int = document.getElementsByClassName("interactive");
 
-for(const int of list_int) {//On récupère chaque élément interactif de index.html
-  int.addEventListener("click", () => {//A chaque clic sur un élément on affiche le Pop-Up avec le texte indiqué
-    popUp(int.id)
-  });
-};
 
 
 /**
- * Affiche un Pop-Up avec un texte correpondant à l'ID donné en paramètre
+ * Affiche un Pop-Up avec un texte correpondant à l'ID de l'élément donné en paramètre
  */
 function popUp(id){
   fetch('data/text.json')
@@ -18,15 +14,48 @@ function popUp(id){
     const PopUpTxt = document.getElementById('PopUpTxt');
 
     PopUpTxt.textContent = data[id];//On affecte le texte au PopUp
-    PopUp.style.display = "block";//On affiche le Pop-Up
+    PopUp.style.display = "flex";//On affiche le Pop-Up
+
+    document.body.style.overflow = "hidden";//Bloquer le scroll
 
     //FERMER LE POP-UP
     const closeBtn = document.getElementById('close');
 
-    closeBtn.onclick = () => PopUp.style.display = "none";
+    closeBtn.onclick = () => {PopUp.style.display = "none"; document.body.style.overflow = "auto";}
     window.onclick = (event) => {
-      if (event.target == PopUp) PopUp.style.display = "none";
+      if (event.target == PopUp){ PopUp.style.display = "none"; document.body.style.overflow = "auto"}
     };
 
   })
+};
+
+
+
+/**
+ * Observeur pour l'animation de l'apparition des éléments interactifs
+ */
+const observer = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      anime({
+        targets: entry.target,
+        translateX: [-50, 0],
+        opacity: [0, 1],
+        duration: 1500,
+        easing: 'easeOutExpo'
+      });
+    }
+  });
+});
+
+
+
+/**
+ * Récupérer tous les éléments interactifs et affectent les fonctions correspondantes
+ */
+for(const int of list_int) {//On récupère chaque élément interactif de index.html
+  observer.observe(int);
+  int.addEventListener("click", () => {//A chaque clic sur un élément on affiche le Pop-Up avec le texte indiqué
+    popUp(int.id);
+  });
 };
