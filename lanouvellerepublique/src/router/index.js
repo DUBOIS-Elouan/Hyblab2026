d3d037d4715cdd2c7aff421781bb24dfa1b78e78
Nathan <nathan.domenichini@gmail.com>
2026-03-12 14:50:16 +0100

    official Header

--- lanouvellerepublique/src/router/index.js
+++ lanouvellerepublique/src/router/index.js
@@ -1,6 +1,6 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/ListView.vue'
+import { createRouter, createWebHistory } from 'vue-router' 
 import MapView from '@/views/MapView.vue'
+import ListView from '../views/ListView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -8,7 +8,7 @@ const router = createRouter({
     {
       path: '/',
       name: 'home',
-      component: HomeView,
+      component: ListView,
     },
     {
       path: '/carte',
