320bcf9d93363bead086e36bf235cb3d9f3447e6
Ryan <ryan.lamrous@etu.univ-nantes.fr>
2026-03-12 11:59:19 +0100

    ✨ Réglage de la taille du texte. Et affichage des bases en fonction de la couleur

--- rembobine/public/js/pageProjet.js
+++ rembobine/public/js/pageProjet.js
@@ -1,7 +1,14 @@
 "use strict";
 
 let Institutionnel = null;
+let Mediatique = null;
+let Public = null;
+let Judiciaire = null;
 
+let count_institutionnel = 0;
+let count_mediatique = 0;
+let count_judiciaire = 0;
+let count_public = 0;
 
 function createButtonBox(boxId = "box1", aRow = 1, aColumn = 1) {
   const box = document.createElement("div");
@@ -10,6 +17,10 @@ function createButtonBox(boxId = "box1", aRow = 1, aColumn = 1) {
   box.className = "box";
   box.row = aRow;
   box.column = aColumn;
+  box.color = null; //In which group the box is a part of
+  box.ngroup = null;//It's id in the group
+
+
 
   for (let rowIndex = 0; rowIndex < 2; rowIndex += 1) {
     const row = document.createElement("div");
@@ -19,8 +30,7 @@ function createButtonBox(boxId = "box1", aRow = 1, aColumn = 1) {
       const buttonNumber = rowIndex * 2 + columnIndex + 1;
       const button = document.createElement("button");
       button.id = `button${buttonNumber}`;
-      console.log(typeof(button.id));
-      console.log(button.id);
+
       button.textContent = ` ${buttonNumber} `;
       row.appendChild(button);
     }
@@ -31,10 +41,10 @@ function createButtonBox(boxId = "box1", aRow = 1, aColumn = 1) {
   box.querySelectorAll('button').forEach(option => {
     option.addEventListener('click', async () => {
         // Send the user's choice to our API
-        console.log(option.textContent);
-        // const value = option.textContent;
 
         const value = option.textContent;
+
+
 
         //const box = option.parentElement.parentElement;
         // Remove all buttons
@@ -44,31 +54,84 @@ function createButtonBox(boxId = "box1", aRow = 1, aColumn = 1) {
         box.querySelectorAll('.row').forEach(row => row.remove());
 
         // Change box background color
-
+        const textDisplay = document.createElement('p');
+        textDisplay.id = "base";
         switch (parseInt(value)) {
-          case 1: 
+          case 1:
             box.style.backgroundColor = '#ff2ba3d1'; // Change color as desired
+            box.color = 1;
+            box.ngroup = count_public;
+            count_public ++;
+
+            if (box.ngroup >= Public.length){
+
+              textDisplay.textContent = "Vous avez vu tout les impact !";
+
+            } else{
+
+              textDisplay.textContent = Public[box.ngroup].Base;
+
+            }
+            
             break;
           case 2:
-            box.style.backgroundColor = '#fff700d1'; // Change color as desired 
+            box.style.backgroundColor = '#fff700d1'; // Change color as desired
+            box.color = 2;
+            box.ngroup = count_mediatique;
+            count_mediatique ++;
+
+            if (box.ngroup >= Mediatique.length){
+
+              textDisplay.textContent = "Vous avez vu tout les impact !";
+
+            } else{
+
+              textDisplay.textContent = Mediatique[box.ngroup].Base;
+
+            }
+
             break;
           case 3:
             box.style.backgroundColor = '#643ff793';
+            box.color = 3;
+            box.ngroup = count_institutionnel;
+            count_institutionnel ++;
+
+            if (box.ngroup >= Institutionnel.length){
+
+              textDisplay.textContent = "Vous avez vu tout les impact !";
+
+            } else{
+
+              textDisplay.textContent = Institutionnel[box.ngroup].Base;
+
+            }
+            
             break;
           case 4:
             box.style.backgroundColor = '#2bff55d1';
+            box.color = 4;
+            box.ngroup = count_judiciaire;
+            count_judiciaire ++;
+
+            if (box.ngroup >= Judiciaire.length){
+
+              textDisplay.textContent = "Vous avez vu tout les impact !";
+
+            } else{
+
+              textDisplay.textContent = Judiciaire[box.ngroup].Base;
+
+            }
+
             break;
           default: 
             box.style.backgroundColor = '#d5d5d5d1';
             break;
         }
-          
         // Display clicked button text
-        const textDisplay = document.createElement('p');
-        console.log(Institutionnel[option.id])
-        textDisplay.textContent = Institutionnel[parseInt(option.textContent,10) - 1].Base;
-        console.log(textDisplay.textContent);
         box.appendChild(textDisplay);
+        console.log(textDisplay);
 
         if(getBoxByPosition(box.row + 1, box.column) == null) {
           console.log("====================== add row ==========================");
@@ -79,12 +142,8 @@ function createButtonBox(boxId = "box1", aRow = 1, aColumn = 1) {
 
         const boxNum = Math.random() * (boxsFreeList.length - 0) + 0;
 
-        console.log(boxNum);
-        console.log(boxsFreeList);
-
         let theChoosenBox = boxsFreeList[parseInt(boxNum)];
 
-        console.log(theChoosenBox);
 
         replaceBox(theChoosenBox, createButtonBox(`box${theChoosenBox.row}${theChoosenBox.column}`, theChoosenBox.row, theChoosenBox.column));
 
@@ -154,14 +213,13 @@ function getFreeAdgacentBox(box) {
     boxList.push(boxRight);
   }
 
-  console.log(boxList.length);
 
   if (boxList.length == 0) {
     let boxs = document.querySelectorAll('.box');
 
     console.log("================================================================================================");
 
-    console.log(boxFree);
+
 
     for (const box of boxs) {
       if(box.isFree) {
@@ -188,8 +246,11 @@ const initPageProjet = async function () {
   const response2 = (await fetch('/rembobine/data/article.json'));
   const article = await response2.json()
   Institutionnel = article.Institutionnel;
-  console.log(article);
-  console.log(Institutionnel);
+  Mediatique = article.Mediatique;
+  Public = article.Public;
+  Judiciaire = article.Judiciaire;
+
+
 
 
   const titre = document.getElementById('titre');
