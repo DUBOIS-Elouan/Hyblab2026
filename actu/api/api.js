4abe3294f23213e88ae5dc8e06a9850e451163d1
Bryan <bryan.levy@etu.univ-nantes.fr>
2026-03-13 15:35:43 +0100

    CHORE - Ajout de configuration du proxy

--- actu/api/api.js
+++ actu/api/api.js
@@ -7,6 +7,18 @@ const cookieParser = require("cookie-parser");
 const crypto = require("crypto");
 const linkedom = require("linkedom");
 const fs = require("fs");
+const globalagent = require('global-agent');
+
+// Définition des variables HTTP_PROXY si elle ne le sont pas
+// et que http_proxy l'est
+if ( process.env.HTTP_PROXY === undefined && process.env.http_proxy !== undefined)
+    process.env.HTTP_PROXY = process.env.http_proxy;
+if ( process.env.HTTPS_PROXY === undefined && process.env.http_proxy !== undefined)
+    process.env.HTTPS_PROXY = process.env.https_proxy;
+
+// permet aux requêtes fetch de fonctionner même si un proxy est configuré (réseau de l'université)
+globalagent.bootstrap({environmentVariableNamespace: ''});
+
 
 app.use(cookieParser());
 
